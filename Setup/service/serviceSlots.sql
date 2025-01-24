dc441f768f0f0c3fbbf7901f2f4f44f216eb4860
Blake Bertuccelli-Booth <46652+bbertucc@users.noreply.github.com>
2025-01-24 11:38:03 -0600

    Delete Setup/service/serviceSlots.sql

--- Setup/service/serviceSlots.sql
+++
@@ -1,9 +0,0 @@
-SELECT 
-    GuestServices.service_id,
-    GuestServices.slot_id,
-    GuestServices.guest_id
-FROM 
-    GuestServices
-WHERE 
-    GuestServices.service_id = @service_id
-    AND GuestServices.status = 'Slotted';
