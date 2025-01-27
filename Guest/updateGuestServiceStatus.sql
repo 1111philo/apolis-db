43c0d4d578545cf624ef1862b58195d4a3579748
Blake Bertuccelli-Booth <46652+bbertucc@users.noreply.github.com>
2025-01-27 16:38:32 -0600

    Update updateGuestServiceStatus.sql

--- Guest/updateGuestServiceStatus.sql
+++ Guest/updateGuestServiceStatus.sql
@@ -1,9 +1,22 @@
 UPDATE GuestServices
 SET 
-    status = @new_status, -- Update the status to the new value
-    queued_at = CASE WHEN @new_status = 'Queued' THEN CURRENT_TIMESTAMP ELSE queued_at END,
-    slotted_at = CASE WHEN @new_status = 'Slotted' THEN CURRENT_TIMESTAMP ELSE slotted_at END,
-    completed_at = CASE WHEN @new_status = 'Completed' THEN CURRENT_TIMESTAMP ELSE completed_at END
+    status = @new_status,
+    slot_id = CASE 
+        WHEN @new_status = 'Slotted' THEN @slot_id 
+        ELSE slot_id 
+    END,
+    queued_at = CASE 
+        WHEN @new_status = 'Queued' THEN CURRENT_TIMESTAMP 
+        ELSE queued_at 
+    END,
+    slotted_at = CASE 
+        WHEN @new_status = 'Slotted' THEN CURRENT_TIMESTAMP 
+        ELSE slotted_at 
+    END,
+    completed_at = CASE 
+        WHEN @new_status = 'Completed' THEN CURRENT_TIMESTAMP 
+        ELSE completed_at 
+    END
 WHERE 
     guest_id = @guest_id
     AND service_id = @service_id;
