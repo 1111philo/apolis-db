54eec36a7947577a9755606ca76621e7bc5a4e2a
Blake Bertuccelli-Booth <blakebertuccelli@gmail.com>
2025-01-29 11:21:45 -0600

    Add slot number

--- Service/serviceGuestsSlotted.sql
+++ Service/serviceGuestsSlotted.sql
@@ -5,6 +5,7 @@ SELECT
     Guests.dob,
     Guests.case_manager,
     GuestServices.service_id,
+    GuestServices.slot_id,
     GuestServices.status,
     GuestServices.queued_at
 FROM 
@@ -13,9 +14,9 @@ INNER JOIN
     GuestServices ON Guests.guest_id = GuestServices.guest_id
 WHERE 
     GuestServices.status = 'Slotted'
--- conditions to limit with search
+-- Optional conditions to limit search
 -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
     -- AND (
-    --     (@service_id IS NOT NULL AND GuestServices.service_id = @service_id) 
-    --     AND (@guest_id IS NOT NULL AND Guests.guest_id = @guest_id)
+    --     (@service_id IS NULL OR GuestServices.service_id = @service_id) 
+    --     AND (@guest_id IS NULL OR Guests.guest_id = @guest_id)
     -- );
